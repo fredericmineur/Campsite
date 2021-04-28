03365c4c3eb17c1956e5078e9fe25175dbb68715
Frederic Mineur <fredericmineur@gmail.com>
2021-04-28 17:55:50 +0100

    add campground new and create functionality

--- app.js
+++ app.js
@@ -21,6 +21,8 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.urlencoded({extended:true}));
+
 
 
 app.get('/', (req, res) => {
@@ -32,12 +34,22 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', {campgrounds})
 })
 
+app.get('/campgrounds/new', (req, res) => {
+    res.render('campgrounds/new');
+})
+
+app.post('/campgrounds', async (req, res) => {
+    const campground = new Campground(req.body.campground);
+    await campground.save();
+    res.redirect(`/campgrounds/${campground._id}`);
+})
+
 app.get('/campgrounds/:id',  async (req, res) => {
     const {id} = req.params;
-    console.log(id);
     const campground = await Campground.findById(id);
     res.render('campgrounds/show', {campground})
 })
+
 
 
 app.listen(3000, () => {console.log('Serving on port 3000')})
