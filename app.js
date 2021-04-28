4e4b58e0390c3741fd0321abec8042540224aef9
Frederic Mineur <fredericmineur@gmail.com>
2021-04-28 19:29:43 +0100

    add edit campground functionality

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const path = require('path');
 const mongoose = require('mongoose');
 const Campground = require('./models/campground');
 const { render } = require('ejs');
+const methodOverride = require('method-override');
 
 mongoose.connect('mongodb://localhost:27017/camp-site', {
     useNewUrlParser: true,
@@ -22,6 +23,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({extended:true}));
+app.use(methodOverride('_method'));
 
 
 
@@ -50,6 +52,16 @@ app.get('/campgrounds/:id',  async (req, res) => {
     res.render('campgrounds/show', {campground})
 })
 
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const campground = await Campground.findById(req.params.id);
+    res.render('campgrounds/edit', {campground});
+})
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const campground = await Campground.findByIdAndUpdate(req.params.id, 
+        {...req.body.campground}, {runValidators:true, new:true, useFindAndModify:false});
+    res.redirect(`/campgrounds/${req.params.id}`);
+})
 
 
 app.listen(3000, () => {console.log('Serving on port 3000')})
