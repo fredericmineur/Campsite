c3f163f3b2c7ea005e44ae7717baedb08a810625
Frederic Mineur <fredericmineur@gmail.com>
2021-04-28 16:31:11 +0100

    add seeding/fixture functionality

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-const Campground = require('./models/campgound');
+const Campground = require('./models/campground');
 
 mongoose.connect('mongodb://localhost:27017/camp-site', {
     useNewUrlParser: true,
