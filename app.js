d0307ca7197bc12967afedd37f69ce0f55ece266
Frederic Mineur <fredericmineur@gmail.com>
2021-05-16 14:01:27 +0100

    add static assets and public folder

--- app.js
+++ app.js
@@ -1,14 +1,8 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
-// const { render } = require('ejs');
 const ejsMate = require('ejs-mate');
 const methodOverride = require('method-override');
-const catchAsync = require('./utils/catchAsync');
-
-const { campgroundSchema, reviewSchema } = require('./schemas');
-const Campground = require('./models/campground');
-const Review = require('./models/review');
 const ExpressError = require('./utils/ExpressError');
 const campgroundRoutes = require('./routes/campground');
 const reviewRoutes = require('./routes/review');
@@ -27,37 +21,16 @@ db.once("open", () => {
 
 const app = express();
 
-
-
 app.engine('ejs', ejsMate);
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname,'public')));
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));
 
 app.use('/campgrounds', campgroundRoutes);
 app.use('/campgrounds/:id/reviews', reviewRoutes );
-
-const validateCampground = (req, res, next) => {
-    const { error } = campgroundSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        next(new ExpressError(msg, 400));
-    } else {
-        next();
-    }
-}
-
-const validateReview = (req, res, next) => {
-    const { error } = reviewSchema.validate(req.body);
-    if (error) {
-        const msg = error.details.map(el => el.message).join(',');
-        next(new ExpressError(msg, 400));
-    } else {
-        next();
-    }
-}
 
 
 
@@ -65,15 +38,6 @@ app.get('/', (req, res) => {
     res.render('home');
 })
 
-
-
-
-//CAMPGROUNDS
-
-
-
-
-//REVIEWS
 
 
 
