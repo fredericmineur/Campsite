ede7b8b23a9e6e60caec0613258008c418cac478
Frederic Mineur <fredericmineur@gmail.com>
2021-05-19 19:43:03 +0100

    add logic to login with passport

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,9 @@
 const express = require('express');
+const passport = require('passport');
 const router = express.Router();
 const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
+// const { route } = require('./campgrounds');
 
 router.get('/register', (req, res) =>{
     res.render('users/register');
@@ -20,5 +22,16 @@ router.post('/register', catchAsync (async (req, res)=>{
         res.redirect('/register');
     }
 }));
+
+router.get('/login', (req, res) =>{
+    res.render('users/login')
+});
+
+router.post('/login', 
+    passport.authenticate('local', 
+        {failureRedirect:'/login', failureFlash:'invalid credentials'}),
+    (req, res) =>{
+        res.redirect('/campgrounds');
+})
 
 module.exports = router;
