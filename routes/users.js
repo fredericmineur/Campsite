8467becfaf7f684a40e5a4fb08a36c1cf1d016a9
Frederic Mineur <fredericmineur@gmail.com>
2021-05-21 02:19:20 +0100

    add user controller

--- routes/users.js
+++ routes/users.js
@@ -1,48 +1,22 @@
 const express = require('express');
 const passport = require('passport');
 const router = express.Router();
-const User = require('../models/user');
 const catchAsync = require('../utils/catchAsync');
-// const { route } = require('./campgrounds');
 
-router.get('/register', (req, res) => {
-    res.render('users/register');
-})
+const userController = require('../controllers/users');
 
-router.post('/register', catchAsync(async (req, res, next) => {
-    try {
-        const { username, email, password } = req.body;
-        const user = new User({ username, email });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, (err) => {
-            if (err) { return next(err); }
-            req.flash('success', 'welcome to Campground');
-            res.redirect('/campgrounds');
-        })
-    } catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/register');
-    }
-}));
+router.get('/register', userController.renderRegisterForm);
 
-router.get('/login', (req, res) => {
-    res.render('users/login')
-});
+router.post('/register', catchAsync(userController.register));
+
+router.get('/login', userController.renderLogin);
 
 router.post('/login',
     passport.authenticate('local',
-        { failureRedirect: '/login', failureFlash: 'invalid credentials' }),
-    (req, res) => {
-        req.flash('success', 'Welcome back');
-        const urlToDirect = req.session.returnTo || '/campgrounds';
-        delete req.session.returnTo
-        res.redirect(urlToDirect);
-    })
+        { failureRedirect: '/login', 
+        failureFlash: 'invalid credentials' }),
+    userController.login)
 
-router.get('/logout', (req, res) => {
-    req.logout();
-    req.flash('success', 'Goodbye');
-    res.redirect('/campgrounds');
-})
+router.get('/logout', userController.logout)
 
 module.exports = router;
